a7a2b2d29a5189023e676d919ca0e13f6740acc0
dianfitrip <dianfitri683@gmail.com>
2026-01-02 20:15:08 +0700

    menambahkan data class Siswa sebagai model data siswa

---
+++ app/src/main/java/com/example/myapplication/modeldata/siswa.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.modeldata
+
+
+data class Siswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
