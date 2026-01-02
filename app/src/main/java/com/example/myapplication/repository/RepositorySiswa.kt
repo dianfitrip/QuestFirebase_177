2a0e5bd0c388a2f7cae5e7a09a323f2b63aa12a7
dianfitrip <dianfitri683@gmail.com>
2026-01-02 20:25:18 +0700

    menambahkan FirebaseRepositorySiswa untuk akses data siswa di Firestore

--- app/src/main/java/com/example/myapplication/repository/RepositorySiswa.kt
+++ app/src/main/java/com/example/myapplication/repository/RepositorySiswa.kt
@@ -9,3 +9,10 @@ interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
 }
+
+class FirebaseRepositorySiswa : RepositorySiswa {
+    private val db = FirebaseFirestore.getInstance()
+    private val collection = db.collection("siswa")
+
+
+}
