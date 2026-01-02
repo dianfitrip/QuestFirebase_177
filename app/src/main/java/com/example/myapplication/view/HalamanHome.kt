defd7c9a851a33c723107ec10dc9b752faddeb40
dianfitrip <dianfitri683@gmail.com>
2026-01-02 22:04:53 +0700

    menambahkan LoadingScreen untuk menampilkan indikator pemuatan data

--- app/src/main/java/com/example/myapplication/view/HalamanHome.kt
+++ app/src/main/java/com/example/myapplication/view/HalamanHome.kt
@@ -107,3 +107,12 @@ fun HomeBody(
     }
 }
 
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_icon),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
