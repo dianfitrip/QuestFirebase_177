2e07cd56535be166dc7743814705d495b2ac1922
dianfitrip <dianfitri683@gmail.com>
2026-01-02 22:20:20 +0700

    menambahkan HomeViewModel untuk memuat dan menampilkan data siswa

--- app/src/main/java/com/example/myapplication/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/HomeViewModel.kt
@@ -15,3 +15,26 @@ sealed interface StatusUiSiswa {
     object Error : StatusUiSiswa
     object Loading : StatusUiSiswa
 }
+class HomeViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() {
+    var statusUiSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init{
+        loadSiswa()
+    }
+
+    fun loadSiswa(){
+        viewModelScope.launch {
+            statusUiSiswa = StatusUiSiswa.Loading
+            statusUiSiswa = try {
+                StatusUiSiswa.Success(repositorySiswa.getDataSiswa())
+            }catch (e:IOException){
+                StatusUiSiswa.Error
+            }
+            catch (e:Exception){
+                StatusUiSiswa.Error
+            }
+        }
+    }
+
+}
