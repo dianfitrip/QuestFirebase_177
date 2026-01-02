01c7bad91e2c541ff92add7e0fe18923c321f77d
dianfitrip <dianfitri683@gmail.com>
2026-01-02 22:16:22 +0700

    menambahkan DestinasiEntry sebagai rute navigasi halaman entry siswa

---
+++ app/src/main/java/com/example/myapplication/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.view.route
+
+import com.example.myapplication.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
