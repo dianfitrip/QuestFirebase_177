6c2131e8edbff4042faf871494823ae9721e9566
dianfitrip <dianfitri683@gmail.com>
2026-01-02 21:30:05 +0700

    menambahkan helper CreationExtras untuk mengambil AplikasiDataSiswa

---
+++ app/src/main/java/com/example/myapplication/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myapplication.repository.AplikasiDataSiswa
+
+fun CreationExtras.aplikasiDataSiswa():AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+                AplikasiDataSiswa
+        )
