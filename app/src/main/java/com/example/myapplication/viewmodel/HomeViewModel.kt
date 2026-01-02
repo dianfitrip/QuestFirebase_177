d0c23af09b259759671c5bf4499a6df319d1673c
dianfitrip <dianfitri683@gmail.com>
2026-01-02 22:19:38 +0700

    menambahkan StatusUiSiswa untuk mengelola status tampilan data siswa

---
+++ app/src/main/java/com/example/myapplication/viewmodel/HomeViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import java.io.IOException
+import com.example.myapplication.modeldata.Siswa
+import com.example.myapplication.repository.RepositorySiswa
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
