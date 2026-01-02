59a237003892803897e33754ccf2eb497dd00eaa
dianfitrip <dianfitri683@gmail.com>
2026-01-02 21:25:01 +0700

    menambahkan DataSiswaApp sebagai pengatur navigasi utama aplikasi

---
+++ app/src/main/java/com/example/myapplication/view/controllNavigasi/PetaNavigasi.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication.view.controllNavigasi
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.view.EntrySiswaScreen
+import com.example.myapplication.view.HomeScreen
+import com.example.myapplication.view.route.DestinasiDetail
+import com.example.myapplication.view.route.DestinasiEntry
+import com.example.myapplication.view.route.DestinasiHome
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier:
+Modifier){
+    HostNavigasi(navController = navController)
+}
+
