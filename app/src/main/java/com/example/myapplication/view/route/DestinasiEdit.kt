cc062bfc1d33ad33da2a109cd4f21b6f2eb69270
dianfitrip <dianfitri683@gmail.com>
2026-01-02 22:15:30 +0700

    menambahkan DestinasiEdit sebagai rute navigasi halaman edit siswa

---
+++ app/src/main/java/com/example/myapplication/view/route/DestinasiEdit.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.view.route
+
+import com.example.myapplication.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
