fa1c75632340cf685d74231e44e82746ee555b7b
dianfitrip <dianfitri683@gmail.com>
2026-01-02 22:17:14 +0700

    menambahkan DestinasiHome sebagai rute navigasi halaman utama

---
+++ app/src/main/java/com/example/myapplication/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.view.route
+
+import com.example.myapplication.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
