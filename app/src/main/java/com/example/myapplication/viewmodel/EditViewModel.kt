24052a3ca62761ee9c3efdc0834e0ac58030afdf
dianfitrip <dianfitri683@gmail.com>
2026-01-09 16:36:33 +0700

    menambahkan updateUiState dan validasi input pada EditViewModel

--- app/src/main/java/com/example/myapplication/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/EditViewModel.kt
@@ -18,5 +18,16 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySi
         }
     }
 
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
 
 }
