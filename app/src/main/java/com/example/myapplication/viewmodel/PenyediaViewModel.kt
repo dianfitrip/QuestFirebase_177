2386d3f60ef8287d775e2d0ac5be09622f628fc9
dianfitrip <dianfitri683@gmail.com>
2026-01-09 16:29:29 +0700

    menambahkan initializer EditViewModel pada PenyediaViewModel

--- app/src/main/java/com/example/myapplication/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/PenyediaViewModel.kt
@@ -15,7 +15,7 @@ object PenyediaViewModel {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { DetailViewModel(this.createSavedStateHandle(),aplikasiDataSiswa().container.repositorySiswa) }
-
+        initializer { EditViewModel(this.createSavedStateHandle(),aplikasiDataSiswa().container.repositorySiswa) }
     }
 }
 
