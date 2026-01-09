bd9815f049c6898bd0dd45c18d811b59fc9e4132
dianfitrip <dianfitri683@gmail.com>
2026-01-09 16:13:41 +0700

    menambahkan navigasi EditSiswaScreen dengan parameter itemId

--- app/src/main/java/com/example/myapplication/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myapplication/view/controllNavigasi/PetaNavigasi.kt
@@ -44,5 +44,11 @@ fun HostNavigasi(
             DetailSiswaScreen(navigateToEditItem = {navController.navigate("${DestinasiEdit.route}/$it")},
                 navigateBack = { navController.navigate(DestinasiHome.route) })
         }
+        composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument
+            (DestinasiEdit.itemIdArg){
+            type = NavType.StringType})){
+            EditSiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() })
+        }
     }
 }
